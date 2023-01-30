784a72bd7e4333105608fb74a618e65615813773
Tyler Nguyen <40559762+TylerXNguyen@users.noreply.github.com>
2023-01-29 21:58:02 -0800

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,5 @@
--- Average person by establishment (population / establishments)
+-- Table 1
+-- Average person by establishment (population / establishments) without Totals
 WITH combined_salpop AS (
 	SELECT 
 		pop.County, pop.population, Quarterly_Establishments, 
@@ -8,16 +9,28 @@ WITH combined_salpop AS (
 	JOIN Portfolio_salaryAnalysis.dbo.salary sal ON pop.County = sal.County
 )
 SELECT *, CAST(Round((1.0 * population / Quarterly_Establishments), 4) AS DECIMAL(9, 2)) AS avg_persons_per_establishment
+INTO #popCombinedSal
 FROM combined_salpop
+WHERE County NOT IN ('U.S. TOTAL', 'California')
 
--- Create Table for Pop combined
-Create Table #popCombinedSal 
-(
-County nvarchar(255),
-population numeric,
-Quarterly_establishments numeric,
-Average_Weekly_Wage numeric,
-avg_persons_per_establishment numeric
+-- Table 2
+-- Average person by establishment (population / establishments) with Totals
+WITH combined_salpop AS (
+	SELECT 
+		pop.County, pop.population, Quarterly_Establishments, 
+		January_Employment, February_Employment, March_Employment, 
+		Total_Quarterly_Wages, Average_Weekly_Wage
+	FROM Portfolio_salaryAnalysis.dbo.population pop
+	JOIN Portfolio_salaryAnalysis.dbo.salary sal ON pop.County = sal.County
 )
+SELECT *, CAST(Round((1.0 * population / Quarterly_Establishments), 4) AS DECIMAL(9, 2)) AS avg_persons_per_establishment
+INTO #popCombinedSalTotals
+FROM combined_salpop
+WHERE County IN ('U.S. TOTAL', 'California')
 
-DROP TABLE if exists #popCombinedSal
+-- Delete Table for Pop combined
+DROP TABLE if exists #popCombinedSal;
+-- Selecting alls
+SELECT * FROM #popCombinedSal;
+SELECT * FROM #popCombinedSalTotals;
+
